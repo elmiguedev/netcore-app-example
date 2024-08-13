68864897393c034fb2531bbb2f97e7284f0c8e86
migue <mmoyano16@gmail.com>
2024-08-13 07:46:41 -0300

    headers empleados

--- src/core/services/EmpleadoService.ts
+++ src/core/services/EmpleadoService.ts
@@ -1,6 +1,13 @@
+import { AuthService } from "./AuthService";
+
 export class EmpleadoService {
   public static async getEmpleados(): Promise<any> {
     const url = "http://localhost:5151/empleados";
+    // const headers = {
+    //   'Content-Type': 'application/json',
+    //   'Authorization': `Bearer ${AuthService.getToken()}`
+    // }
+    // const response = await fetch(url, { headers });
     const response = await fetch(url);
     return response.json();
   }
